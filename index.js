0f4e0fdd7bacc04c8d37750c3bc4217d4baab0d4
GeoReece000 <geofreytegeret@gmail.com>
2023-03-31 16:22:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function uppercaseCustomerName() {
+
+   const upperCase =  customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    
+    var bestCustomer = 'not bob'
+}
+
+setBestCustomer();
+
+function overwriteBestCustomer(name) {
+
+    var bestCustomer = 'maybe bob'
+}
+
+overwriteBestCustomer(bestCustomer);
+
